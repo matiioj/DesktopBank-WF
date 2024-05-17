d239b62fe7e2781cdcb6ba973e444e4bc86b5036
sebastianadiaz <167219488+sebastianadiaz@users.noreply.github.com>
2024-05-17 17:28:19 -0300

    Se modifica línea en FormRegister Label CUIL
    También Linea 92 y 94, de 1 a 11, a 11 a 11.

--- DesktopBankUI/FormRegister.cs
+++ DesktopBankUI/FormRegister.cs
@@ -89,9 +89,9 @@ namespace StudentSystem.WindowsFormsCliente
                     mensajeError += "El correo electrónico no tiene un formato válido.\n";
                 }
 
-                if (!Regex.IsMatch(cuil, @"^\d{1,11}$"))
+                if (!Regex.IsMatch(cuil, @"^\d{11,11}$"))
                 {
-                    mensajeError += "El CUIL debe contener hasta 11 dígitos numéricos.\n";
+                    mensajeError += "El CUIL debe contener 11 dígitos numéricos.\n";
                 }
             }
 
