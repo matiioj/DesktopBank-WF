6f15f6df11e1ce095b923799c2680bc7172b3c27
sebastianadiaz <167219488+sebastianadiaz@users.noreply.github.com>
2024-05-17 17:34:03 -0300

    Se modifica línea FrmRegister Linea 92 y 94
    Para que ahora en vez de pedir hasta 11 dígitos y dejar colocar menos, sólo permita 11 obligatoriamente.

--- DesktopBankUI/FormRegister.Designer.cs
+++ DesktopBankUI/FormRegister.Designer.cs
@@ -63,105 +63,120 @@
             boxDatos.Controls.Add(labelCorreo);
             boxDatos.Controls.Add(labelApellido);
             boxDatos.Controls.Add(labelNombre);
-            boxDatos.Location = new Point(12, 56);
+            boxDatos.Location = new Point(17, 93);
+            boxDatos.Margin = new Padding(4, 5, 4, 5);
             boxDatos.Name = "boxDatos";
-            boxDatos.Size = new Size(370, 196);
+            boxDatos.Padding = new Padding(4, 5, 4, 5);
+            boxDatos.Size = new Size(529, 327);
             boxDatos.TabIndex = 0;
             boxDatos.TabStop = false;
             // 
             // TxtCuil
             // 
-            TxtCuil.Location = new Point(86, 100);
+            TxtCuil.Location = new Point(123, 167);
+            TxtCuil.Margin = new Padding(4, 5, 4, 5);
             TxtCuil.Name = "TxtCuil";
-            TxtCuil.Size = new Size(265, 23);
+            TxtCuil.Size = new Size(377, 31);
             TxtCuil.TabIndex = 11;
+            TxtCuil.TextChanged += TxtCuil_TextChanged;
             // 
             // labelCuil
             // 
             labelCuil.AutoSize = true;
-            labelCuil.Location = new Point(49, 108);
+            labelCuil.Location = new Point(70, 180);
+            labelCuil.Margin = new Padding(4, 0, 4, 0);
             labelCuil.Name = "labelCuil";
-            labelCuil.Size = new Size(31, 15);
+            labelCuil.Size = new Size(45, 25);
             labelCuil.TabIndex = 10;
             labelCuil.Text = "Cuil:";
             // 
             // TxtContra
             // 
-            TxtContra.Location = new Point(86, 152);
+            TxtContra.Location = new Point(123, 253);
+            TxtContra.Margin = new Padding(4, 5, 4, 5);
             TxtContra.Name = "TxtContra";
-            TxtContra.Size = new Size(265, 23);
+            TxtContra.Size = new Size(377, 31);
             TxtContra.TabIndex = 9;
             // 
             // labelContra
             // 
             labelContra.AutoSize = true;
-            labelContra.Location = new Point(10, 160);
+            labelContra.Location = new Point(14, 267);
+            labelContra.Margin = new Padding(4, 0, 4, 0);
             labelContra.Name = "labelContra";
-            labelContra.Size = new Size(70, 15);
+            labelContra.Size = new Size(105, 25);
             labelContra.TabIndex = 8;
             labelContra.Text = "Contraseña:";
             // 
             // TxtUser
             // 
-            TxtUser.Location = new Point(86, 126);
+            TxtUser.Location = new Point(123, 210);
+            TxtUser.Margin = new Padding(4, 5, 4, 5);
             TxtUser.Name = "TxtUser";
-            TxtUser.Size = new Size(265, 23);
+            TxtUser.Size = new Size(377, 31);
             TxtUser.TabIndex = 7;
             // 
             // TxtCorreo
             // 
-            TxtCorreo.Location = new Point(86, 73);
+            TxtCorreo.Location = new Point(123, 122);
+            TxtCorreo.Margin = new Padding(4, 5, 4, 5);
             TxtCorreo.Name = "TxtCorreo";
-            TxtCorreo.Size = new Size(265, 23);
+            TxtCorreo.Size = new Size(377, 31);
             TxtCorreo.TabIndex = 6;
             // 
             // TxtApellido
             // 
-            TxtApellido.Location = new Point(86, 47);
+            TxtApellido.Location = new Point(123, 78);
+            TxtApellido.Margin = new Padding(4, 5, 4, 5);
             TxtApellido.Name = "TxtApellido";
-            TxtApellido.Size = new Size(265, 23);
+            TxtApellido.Size = new Size(377, 31);
             TxtApellido.TabIndex = 5;
             // 
             // TxtNombre
             // 
-            TxtNombre.Location = new Point(86, 21);
+            TxtNombre.Location = new Point(123, 35);
+            TxtNombre.Margin = new Padding(4, 5, 4, 5);
             TxtNombre.Name = "TxtNombre";
-            TxtNombre.Size = new Size(265, 23);
+            TxtNombre.Size = new Size(377, 31);
             TxtNombre.TabIndex = 4;
             // 
             // labelUsuario
             // 
             labelUsuario.AutoSize = true;
-            labelUsuario.Location = new Point(30, 134);
+            labelUsuario.Location = new Point(43, 223);
+            labelUsuario.Margin = new Padding(4, 0, 4, 0);
             labelUsuario.Name = "labelUsuario";
-            labelUsuario.Size = new Size(50, 15);
+            labelUsuario.Size = new Size(76, 25);
             labelUsuario.TabIndex = 3;
             labelUsuario.Text = "Usuario:";
             // 
             // labelCorreo
             // 
             labelCorreo.AutoSize = true;
-            labelCorreo.Location = new Point(34, 81);
+            labelCorreo.Location = new Point(49, 135);
+            labelCorreo.Margin = new Padding(4, 0, 4, 0);
             labelCorreo.Name = "labelCorreo";
-            labelCorreo.Size = new Size(46, 15);
+            labelCorreo.Size = new Size(70, 25);
             labelCorreo.TabIndex = 2;
             labelCorreo.Text = "Correo:";
             // 
             // labelApellido
             // 
             labelApellido.AutoSize = true;
-            labelApellido.Location = new Point(26, 55);
+            labelApellido.Location = new Point(37, 92);
+            labelApellido.Margin = new Padding(4, 0, 4, 0);
             labelApellido.Name = "labelApellido";
-            labelApellido.Size = new Size(54, 15);
+            labelApellido.Size = new Size(82, 25);
             labelApellido.TabIndex = 1;
             labelApellido.Text = "Apellido:";
             // 
             // labelNombre
             // 
             labelNombre.AutoSize = true;
-            labelNombre.Location = new Point(26, 29);
+            labelNombre.Location = new Point(37, 48);
+            labelNombre.Margin = new Padding(4, 0, 4, 0);
             labelNombre.Name = "labelNombre";
-            labelNombre.Size = new Size(54, 15);
+            labelNombre.Size = new Size(82, 25);
             labelNombre.TabIndex = 0;
             labelNombre.Text = "Nombre:";
             // 
@@ -170,9 +185,10 @@
             BtnCancelar.Cursor = Cursors.Hand;
             BtnCancelar.Image = (Image)resources.GetObject("BtnCancelar.Image");
             BtnCancelar.ImageAlign = ContentAlignment.MiddleLeft;
-            BtnCancelar.Location = new Point(214, 272);
+            BtnCancelar.Location = new Point(306, 453);
+            BtnCancelar.Margin = new Padding(4, 5, 4, 5);
             BtnCancelar.Name = "BtnCancelar";
-            BtnCancelar.Size = new Size(107, 29);
+            BtnCancelar.Size = new Size(153, 48);
             BtnCancelar.TabIndex = 1;
             BtnCancelar.Text = "Limpiar";
             BtnCancelar.UseVisualStyleBackColor = true;
@@ -181,9 +197,10 @@
             // BtnAceptar
             // 
             BtnAceptar.Cursor = Cursors.Hand;
-            BtnAceptar.Location = new Point(61, 272);
+            BtnAceptar.Location = new Point(87, 453);
+            BtnAceptar.Margin = new Padding(4, 5, 4, 5);
             BtnAceptar.Name = "BtnAceptar";
-            BtnAceptar.Size = new Size(103, 29);
+            BtnAceptar.Size = new Size(147, 48);
             BtnAceptar.TabIndex = 2;
             BtnAceptar.Text = "Aceptar";
             BtnAceptar.UseVisualStyleBackColor = true;
@@ -192,18 +209,20 @@
             // labelTenesCuenta
             // 
             labelTenesCuenta.AutoSize = true;
-            labelTenesCuenta.Location = new Point(35, 317);
+            labelTenesCuenta.Location = new Point(50, 528);
+            labelTenesCuenta.Margin = new Padding(4, 0, 4, 0);
             labelTenesCuenta.Name = "labelTenesCuenta";
-            labelTenesCuenta.Size = new Size(122, 15);
+            labelTenesCuenta.Size = new Size(183, 25);
             labelTenesCuenta.TabIndex = 3;
             labelTenesCuenta.Text = "¿Ya tenes una cuenta?";
             // 
             // botonLogin
             // 
             botonLogin.Cursor = Cursors.Hand;
-            botonLogin.Location = new Point(42, 344);
+            botonLogin.Location = new Point(60, 573);
+            botonLogin.Margin = new Padding(4, 5, 4, 5);
             botonLogin.Name = "botonLogin";
-            botonLogin.Size = new Size(75, 23);
+            botonLogin.Size = new Size(107, 38);
             botonLogin.TabIndex = 4;
             botonLogin.Text = "Login";
             botonLogin.UseVisualStyleBackColor = true;
@@ -211,14 +230,15 @@
             // 
             // FormRegister
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(399, 395);
+            ClientSize = new Size(570, 658);
             Controls.Add(botonLogin);
             Controls.Add(labelTenesCuenta);
             Controls.Add(BtnAceptar);
             Controls.Add(BtnCancelar);
             Controls.Add(boxDatos);
+            Margin = new Padding(4, 5, 4, 5);
             Name = "FormRegister";
             StartPosition = FormStartPosition.CenterScreen;
             Text = "Registrese";
--- DesktopBankUI/FormRegister.cs
+++ DesktopBankUI/FormRegister.cs
@@ -89,9 +89,9 @@ namespace StudentSystem.WindowsFormsCliente
                     mensajeError += "El correo electrónico no tiene un formato válido.\n";
                 }
 
-                if (!Regex.IsMatch(cuil, @"^\d{1,11}$"))
+                if (!Regex.IsMatch(cuil, @"^\d{11,11}$"))
                 {
-                    mensajeError += "El CUIL debe contener hasta 11 dígitos numéricos.\n";
+                    mensajeError += "El CUIL debe contener 11 dígitos numéricos.\n";
                 }
             }
 
@@ -132,6 +132,11 @@ namespace StudentSystem.WindowsFormsCliente
             FormLogin formLogin = new FormLogin();
             formLogin.Show();
             this.Hide(); // oculta FormRegister
+        }
+
+        private void TxtCuil_TextChanged(object sender, EventArgs e)
+        {
+
         }
     }
 
