79e830050404407e71b18d1a139bb060088d69a7
lautixhx <lautaroben@gmail.com>
2024-05-19 23:03:05 -0300

    agregar user y client services

---
+++ DesktopBank.Services/ClientService.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using DesktopBank.BusinessObjects.Generated.Models;
+using DesktopBank.DAL;
+using DesktopBank.DAL.Repositories;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace DesktopBank.Services
+{
+    public class ClientService
+    {
+        private readonly ClientRepository _clientRepository; 
+
+        public ClientService(ClientRepository clientRepository) 
+        {
+            _clientRepository = clientRepository;
+        }
+
+        public void CreateClient(string name, string surname, int cuil, string mail)
+        {
+            Client newClient = new Client
+            {
+                ClientName = name,
+                ClientSurname = surname,
+                ClientCuil = cuil,
+                ClientEmail = mail
+            };
+
+            _clientRepository.CreateClient(newClient);
+        }
+    }
+}
---
+++ DesktopBank.Services/UserService.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using DesktopBank.BusinessObjects.Generated.Models;
+using DesktopBank.DAL;
+using DesktopBank.DAL.Repositories;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace DesktopBank.Services
+{
+    public class UserService
+    {
+        private readonly UserRepository _userRepository;
+
+        public UserService(UserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        public void CreateUser(string username, string password)
+        {
+            User newUser = new User
+            {
+                UserName = username,
+                UserPassword = password
+            };
+
+            _userRepository.CreateUser(newUser);
+        }
+    }
+}
