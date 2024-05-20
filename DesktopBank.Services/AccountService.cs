5aae4ed34259979f446ea3017eea5f8f96904c51
matias <matiiojj@gmail.com>
2024-05-19 22:18:46 -0300

    add AccountService

---
+++ DesktopBank.Services/AccountService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using DesktopBank.BusinessObjects.Generated.Models;
+using DesktopBank.DAL;
+using DesktopBank.DAL.Repositories;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace DesktopBank.Services
+{
+    public class AccountService
+    {
+        private readonly AccountRepository _accountRepository;
+        public AccountService(AccountRepository accountRepository)
+        {
+            _accountRepository = accountRepository;
+        }
+        public void CrearCuenta(string nombre, string apellido, string correo, string cuil, string user, string contra) 
+        {
+
+        }
+    }
+}
