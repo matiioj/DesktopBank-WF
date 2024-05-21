9a044c98fb3f5b3fb3ddc9e2eaaa6ee9a3fcfaeb
matias <matiiojj@gmail.com>
2024-05-21 20:40:35 -0300

    partially add cardService

--- DesktopBank.Services/CreateCardService.cs
+++ DesktopBank.Services/CreateCardService.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using DesktopBank.BusinessObjects.Generated.Models;
+using DesktopBank.BusinessObjects.Interfaces;
+using DesktopBank.DAL;
+using DesktopBank.DAL.Repositories;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +12,32 @@ namespace DesktopBank.Services
 {
     public class CreateCardService
     {
+        private readonly ICardRepository _cardRepository;
+        private readonly UnitOfWork _unitOfWork;
+        private readonly GenerateNumbersService _generateNumbersService;
+        public CreateCardService(ICardRepository cardRepository, UnitOfWork unitOfWork, GenerateNumbersService generateNumbersService) 
+        {
+            _cardRepository = cardRepository;
+            _unitOfWork = unitOfWork;
+            _generateNumbersService = generateNumbersService;
+        }
+
+        public async Task<Card> CreateCardAsync(int accountId, int accountCurrency)
+        {
+            Card newCard = new Card
+            {
+                CardNumber = name,
+                CardAccountId = surname,
+                CardExpirationDate = cuil,
+                CardCvv = (byte)_generateNumbersService.GenerateRandomNumber(3),
+                TypeOfCard = 0,
+                StatusOfCard = 1,
+                CardCurrency = (byte)accountCurrency
+            };
+
+            _cardRepository.CreateCard(newCard);
+            await _unitOfWork.SaveChangesAsync();
+            return newCard;
+        }
     }
 }
---
+++ DesktopBank.Services/GenerateNumbersService.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesktopBank.Services
+{
+    public class GenerateNumbersService
+    {
+        Random _random = new Random();  
+        public GenerateNumbersService() 
+        {
+        }
+
+        public long GenerateRandomNumber(int length)
+        {
+            const string digits = "123456789";
+            var result = new StringBuilder(length);
+
+            for (int i = 0; i < length; i++)
+            {
+                result.Append(digits[_random.Next(digits.Length)]);
+            }
+
+            return long.Parse(result.ToString());
+        }
+
+        public long GenerateRandomNumberForCVV() // hay que cambiar el tipo byte en bdd, momentaneamente se hace el metodo para guardar igual
+        {
+            const string digits = "123456789";
+            var result = new StringBuilder(3);
+
+            for (int i = 0; i < 3; i++)
+            {
+                result.Append(digits[_random.Next(digits.Length)]);
+                if(i == 1) 
+                {
+                    result.Append(digits[_random.Next(5)]);
+                }
+                if (i == 2) 
+                {
+                    result.Append(digits[_random.Next(5)]);
+                }
+            }
+
+            return long.Parse(result.ToString());
+        }
+    }
+}
