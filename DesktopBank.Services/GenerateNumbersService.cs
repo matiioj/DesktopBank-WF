509035bd737eaba24493e151b7db325aa7363760
matias <matiiojj@gmail.com>
2024-05-24 13:21:53 -0300

    correct generator of cvv

--- DesktopBank.Services/GenerateNumbersService.cs
+++ DesktopBank.Services/GenerateNumbersService.cs
@@ -33,14 +33,17 @@ namespace DesktopBank.Services
 
             for (int i = 0; i < 2; i++)
             {
-                result.Append(digits[_random.Next(digits.Length)]);
                 if(i == 0) 
                 {
                     result.Append(digits[_random.Next(2)]);
                 }
-                if(i == 1) 
+                else if(i == 1) 
                 {
                     result.Append(digits[_random.Next(4)]);
+                }
+                else 
+                {
+                    result.Append(digits[_random.Next(digits.Length)]);
                 }
             }
 
