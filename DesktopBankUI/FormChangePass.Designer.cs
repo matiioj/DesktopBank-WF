9c1f7594eabe59cb5e2c2c06281c5f3bfe5fc0f6
sebastianadiaz <167219488+sebastianadiaz@users.noreply.github.com>
2024-07-28 18:52:39 -0300

    +FormChangePassFuncional

--- DesktopBankUI/FormChangePass.Designer.cs
+++ DesktopBankUI/FormChangePass.Designer.cs
@@ -32,22 +32,29 @@
             etqInsertarCodigo = new Label();
             panel1 = new Panel();
             txtMailChangePass = new TextBox();
-            btnCancelarChP = new Button();
             etqUserChP = new Label();
-            panel2 = new Panel();
-            txtCode = new TextBox();
+            panelCode = new Panel();
             btnAceptarCode = new Button();
+            txtCode = new TextBox();
+            btnCancelarChP = new Button();
             pictureBox1 = new PictureBox();
+            panelNewPass = new Panel();
+            etqPassFirstValidation = new Label();
+            etqPassSecondValidation = new Label();
+            txtPassFirstValidation = new TextBox();
+            txtPassSecondValidation = new TextBox();
+            button1 = new Button();
             panel1.SuspendLayout();
-            panel2.SuspendLayout();
+            panelCode.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            panelNewPass.SuspendLayout();
             SuspendLayout();
             // 
             // btnEnviarCode
             // 
             btnEnviarCode.Font = new Font("Segoe UI", 14F, FontStyle.Regular, GraphicsUnit.Point);
             btnEnviarCode.ForeColor = Color.Black;
-            btnEnviarCode.Location = new Point(245, 126);
+            btnEnviarCode.Location = new Point(344, 146);
             btnEnviarCode.Name = "btnEnviarCode";
             btnEnviarCode.Size = new Size(243, 53);
             btnEnviarCode.TabIndex = 0;
@@ -67,60 +74,44 @@
             // 
             // panel1
             // 
+            panel1.Controls.Add(panelNewPass);
             panel1.Controls.Add(txtMailChangePass);
             panel1.Controls.Add(etqUserChP);
+            panel1.Controls.Add(panelCode);
             panel1.Controls.Add(btnEnviarCode);
-            panel1.Location = new Point(51, 36);
+            panel1.Location = new Point(40, 36);
             panel1.Name = "panel1";
-            panel1.Size = new Size(738, 199);
+            panel1.Size = new Size(932, 579);
             panel1.TabIndex = 3;
             // 
             // txtMailChangePass
             // 
-            txtMailChangePass.Location = new Point(106, 79);
+            txtMailChangePass.Location = new Point(205, 99);
             txtMailChangePass.Name = "txtMailChangePass";
             txtMailChangePass.Size = new Size(535, 31);
             txtMailChangePass.TabIndex = 4;
             txtMailChangePass.TextChanged += txtMailChangePass_TextChanged;
             // 
-            // btnCancelarChP
-            // 
-            btnCancelarChP.Font = new Font("Segoe UI", 14F, FontStyle.Regular, GraphicsUnit.Point);
-            btnCancelarChP.ForeColor = Color.Black;
-            btnCancelarChP.Location = new Point(623, 554);
-            btnCancelarChP.Name = "btnCancelarChP";
-            btnCancelarChP.Size = new Size(166, 61);
-            btnCancelarChP.TabIndex = 4;
-            btnCancelarChP.Text = "Cancelar";
-            btnCancelarChP.UseVisualStyleBackColor = true;
-            btnCancelarChP.Click += btnCancelarChP_Click;
-            // 
             // etqUserChP
             // 
             etqUserChP.AutoSize = true;
             etqUserChP.Font = new Font("Trebuchet MS", 20F, FontStyle.Regular, GraphicsUnit.Point);
-            etqUserChP.Location = new Point(97, 17);
+            etqUserChP.Location = new Point(196, 37);
             etqUserChP.Name = "etqUserChP";
             etqUserChP.Size = new Size(544, 49);
             etqUserChP.TabIndex = 1;
             etqUserChP.Text = "Ingrese su correo electrónico";
             // 
-            // panel2
+            // panelCode
             // 
-            panel2.Controls.Add(btnAceptarCode);
-            panel2.Controls.Add(txtCode);
-            panel2.Controls.Add(etqInsertarCodigo);
-            panel2.Location = new Point(51, 253);
-            panel2.Name = "panel2";
-            panel2.Size = new Size(738, 219);
-            panel2.TabIndex = 4;
-            // 
-            // txtCode
-            // 
-            txtCode.Location = new Point(187, 84);
-            txtCode.Name = "txtCode";
-            txtCode.Size = new Size(344, 31);
-            txtCode.TabIndex = 3;
+            panelCode.Controls.Add(btnAceptarCode);
+            panelCode.Controls.Add(txtCode);
+            panelCode.Controls.Add(etqInsertarCodigo);
+            panelCode.Location = new Point(99, 262);
+            panelCode.Name = "panelCode";
+            panelCode.Size = new Size(738, 219);
+            panelCode.TabIndex = 4;
+            panelCode.Visible = false;
             // 
             // btnAceptarCode
             // 
@@ -132,27 +123,105 @@
             btnAceptarCode.TabIndex = 4;
             btnAceptarCode.Text = "Aceptar";
             btnAceptarCode.UseVisualStyleBackColor = true;
+            btnAceptarCode.Click += btnAceptarCode_Click;
+            // 
+            // txtCode
+            // 
+            txtCode.Location = new Point(187, 84);
+            txtCode.Name = "txtCode";
+            txtCode.Size = new Size(344, 31);
+            txtCode.TabIndex = 3;
+            // 
+            // btnCancelarChP
+            // 
+            btnCancelarChP.Font = new Font("Segoe UI", 14F, FontStyle.Regular, GraphicsUnit.Point);
+            btnCancelarChP.ForeColor = Color.Black;
+            btnCancelarChP.Location = new Point(804, 655);
+            btnCancelarChP.Name = "btnCancelarChP";
+            btnCancelarChP.Size = new Size(166, 61);
+            btnCancelarChP.TabIndex = 4;
+            btnCancelarChP.Text = "Cancelar";
+            btnCancelarChP.UseVisualStyleBackColor = true;
+            btnCancelarChP.Click += btnCancelarChP_Click;
             // 
             // pictureBox1
             // 
             pictureBox1.BackgroundImage = Properties.Resources.iconPig;
             pictureBox1.Image = Properties.Resources.iconPig;
-            pictureBox1.Location = new Point(514, 554);
+            pictureBox1.Location = new Point(720, 655);
             pictureBox1.Name = "pictureBox1";
             pictureBox1.Size = new Size(78, 61);
             pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
             pictureBox1.TabIndex = 5;
             pictureBox1.TabStop = false;
             // 
+            // panelNewPass
+            // 
+            panelNewPass.Controls.Add(button1);
+            panelNewPass.Controls.Add(txtPassSecondValidation);
+            panelNewPass.Controls.Add(txtPassFirstValidation);
+            panelNewPass.Controls.Add(etqPassSecondValidation);
+            panelNewPass.Controls.Add(etqPassFirstValidation);
+            panelNewPass.Location = new Point(58, 17);
+            panelNewPass.Name = "panelNewPass";
+            panelNewPass.Size = new Size(812, 535);
+            panelNewPass.TabIndex = 6;
+            panelNewPass.Visible = false;
+            // 
+            // etqPassFirstValidation
+            // 
+            etqPassFirstValidation.AutoSize = true;
+            etqPassFirstValidation.Font = new Font("Trebuchet MS", 20F, FontStyle.Regular, GraphicsUnit.Point);
+            etqPassFirstValidation.Location = new Point(237, 29);
+            etqPassFirstValidation.Name = "etqPassFirstValidation";
+            etqPassFirstValidation.Size = new Size(357, 49);
+            etqPassFirstValidation.TabIndex = 0;
+            etqPassFirstValidation.Text = "Contraseña nueva:";
+            // 
+            // etqPassSecondValidation
+            // 
+            etqPassSecondValidation.AutoSize = true;
+            etqPassSecondValidation.Font = new Font("Trebuchet MS", 20F, FontStyle.Regular, GraphicsUnit.Point);
+            etqPassSecondValidation.Location = new Point(96, 194);
+            etqPassSecondValidation.Name = "etqPassSecondValidation";
+            etqPassSecondValidation.Size = new Size(659, 49);
+            etqPassSecondValidation.TabIndex = 1;
+            etqPassSecondValidation.Text = "Ingrese nuevamente su contraseña:";
+            // 
+            // txtPassFirstValidation
+            // 
+            txtPassFirstValidation.Location = new Point(227, 111);
+            txtPassFirstValidation.Name = "txtPassFirstValidation";
+            txtPassFirstValidation.Size = new Size(367, 31);
+            txtPassFirstValidation.TabIndex = 2;
+            // 
+            // txtPassSecondValidation
+            // 
+            txtPassSecondValidation.Location = new Point(227, 280);
+            txtPassSecondValidation.Name = "txtPassSecondValidation";
+            txtPassSecondValidation.Size = new Size(367, 31);
+            txtPassSecondValidation.TabIndex = 3;
+            // 
+            // button1
+            // 
+            button1.Font = new Font("Segoe UI", 14F, FontStyle.Regular, GraphicsUnit.Point);
+            button1.ForeColor = Color.Black;
+            button1.Location = new Point(314, 375);
+            button1.Name = "button1";
+            button1.Size = new Size(199, 59);
+            button1.TabIndex = 4;
+            button1.Text = "Confirmar";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
             // FormChangePass
             // 
             AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.Teal;
-            ClientSize = new Size(822, 639);
+            ClientSize = new Size(984, 728);
             Controls.Add(pictureBox1);
             Controls.Add(btnCancelarChP);
-            Controls.Add(panel2);
             Controls.Add(panel1);
             ForeColor = Color.White;
             Name = "FormChangePass";
@@ -161,9 +230,11 @@
             Load += FormChangePass_Load;
             panel1.ResumeLayout(false);
             panel1.PerformLayout();
-            panel2.ResumeLayout(false);
-            panel2.PerformLayout();
+            panelCode.ResumeLayout(false);
+            panelCode.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            panelNewPass.ResumeLayout(false);
+            panelNewPass.PerformLayout();
             ResumeLayout(false);
         }
 
@@ -175,9 +246,15 @@
         private TextBox txtMailChangePass;
         private Button btnCancelarChP;
         private Label etqUserChP;
-        private Panel panel2;
+        private Panel panelCode;
         private Button btnAceptarCode;
         private TextBox txtCode;
         private PictureBox pictureBox1;
+        private Panel panelNewPass;
+        private Label etqPassFirstValidation;
+        private TextBox txtPassSecondValidation;
+        private TextBox txtPassFirstValidation;
+        private Label etqPassSecondValidation;
+        private Button button1;
     }
 }
--- DesktopBankUI/FormChangePass.cs
+++ DesktopBankUI/FormChangePass.cs
@@ -22,8 +22,11 @@ namespace DesktopBankUI
         private readonly MailService mailService = new MailService();
         private readonly NojedaisticDesktopBankContext _context;
         private readonly AccountRepository _accountRepository;
-        private readonly Account _account;
+        private Account _account;
         private readonly PasswordHashingService _passwordHashingService;
+        private string _code;
+        private string _password;
+        private string mensajeError;
 
         public FormChangePass(NojedaisticDesktopBankContext context)
         {
@@ -31,6 +34,9 @@ namespace DesktopBankUI
             _accountRepository = new AccountRepository(_context);
             _passwordHashingService = new PasswordHashingService();
             _validationService = new();
+            Account _account = null;
+            _code = null;
+            mensajeError = null;
             InitializeComponent();
         }
 
@@ -54,14 +60,10 @@ namespace DesktopBankUI
         private void btnEnviarCode_Click(object sender, EventArgs e)
         {
             string validarMail = txtMailChangePass.Text;
-            
-            Account _account = null;
 
             string mailConfirmado;
 
-            string code;
-
-            var mensajeError = _validationService.ValidateOnRegisterFields(validarMail);
+            mensajeError = _validationService.ValidateOnRegisterFields(validarMail);
 
             if (!string.IsNullOrEmpty(mensajeError))
             {
@@ -70,24 +72,21 @@ namespace DesktopBankUI
             }
 
             _account = _accountRepository.GetByEmail(validarMail);
-            if(_account != null)
+            if (_account != null)
             {
                 DialogResult resultado = MessageBox.Show($"¿Enviar código de reestablecimiento de contraseña al correo: {_account.User.Client.ClientEmail}?", "Aceptar", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
                 if (resultado == DialogResult.Yes)
                 {
                     try
                     {
-                        code = new Random().Next().ToString();
-
-                        //_account.User.Client.ClientEmail = correoNuevo;
-                        //_accountRepository.Update(_account);
-                        //_context.SaveChanges();
-                        //MessageBox.Show("Su correo se ha actualizado con éxito");
+                        _code = new Random().Next().ToString();
                         MailData mailData = new MailData();
                         mailData.MailTo = validarMail;
                         mailData.Subject = "Código de restablecimiento de contraseña";
-                        mailData.Body = $"Su código de restablecimiento de la contraseña es: {code}";
+                        mailData.Body = $"Su código de restablecimiento de la contraseña es: {_code}";
                         mailService.SendMail(mailData);
+                        MessageBox.Show("Se ha enviado un código a su correo electrónico", "Enviado", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        panelCode.Visible = true;
                     }
                     catch (Exception ex)
                     {
@@ -106,9 +105,79 @@ namespace DesktopBankUI
                 return;
             }
 
-            
 
 
+
+        }
+
+        private void btnAceptarCode_Click(object sender, EventArgs e)
+        {
+            if (txtCode.Text == _code && _code != null)
+            {
+                panelNewPass.Visible = true;
+            }
+            else
+            {
+                mensajeError = "Código erróneo";
+                MessageBox.Show(mensajeError, "Error de Validación", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string firstValidationPass = txtPassFirstValidation.Text; 
+            string secondValidationPass = txtPassSecondValidation.Text;
+
+            mensajeError = _validationService.ValidationPassword(firstValidationPass);
+            mensajeError = _validationService.ValidationPassword(firstValidationPass);
+
+
+            if (!string.IsNullOrEmpty(mensajeError))
+            {
+                MessageBox.Show(mensajeError, "Error de Validación", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            if (txtPassFirstValidation.Text==txtPassSecondValidation.Text)
+            {
+
+                DialogResult resultado = MessageBox.Show($"¿Quiere cambiar su contraseña?", "Aceptar", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+                if (resultado == DialogResult.Yes)
+                {
+                    try
+                    {
+                        firstValidationPass = _passwordHashingService.HashPassword(firstValidationPass);
+                        _account.User.UserPassword = firstValidationPass;
+                        _accountRepository.Update(_account);
+                        _context.SaveChanges();
+                        MessageBox.Show("Su contraseña se ha actualizado con éxito");
+                        MailData mailData = new MailData();
+                        mailData.MailTo = _account.User.Client.ClientEmail;
+                        mailData.Subject = "Cambio de contraseña ISTIC DesktopBank";
+                        mailData.Body = "Se ha modificado su contraseña";
+                        mailService.SendMail(mailData);
+                        FormLogin formLogin = new FormLogin();
+                        formLogin.Show();
+                        this.Close();
+
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show($"Ocurrió un error al modificar su contraseña: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        if (ex.InnerException != null)
+                        {
+                            MessageBox.Show($"Error: " + ex.InnerException.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        }
+                    }
+                }
+            }
+            else
+            {
+                mensajeError = "Las contraseñas no coinciden";
+                MessageBox.Show(mensajeError, "Error de Validación", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
         }
     }
 }
